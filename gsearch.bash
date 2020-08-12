170f10499da6ba7a65d0554ac0c3da40f6780a49
Pierre Brechet <brechet@mis.mpg.de>
2020-08-12 12:27:47 +0200

    new grid search with mini batch

--- gsearch.bash
+++ gsearch.bash
@@ -9,12 +9,30 @@ cp "$dir/$template" $fname
 sed -i "s/^\(#SBATCH -J\) test_slurm/\1 $name/" $fname
 
 #for n in 36 37 38 39 40 41 42 43 44;
-for n in 39 40 41;
+for n in 30 35 39 40 41 45 50 60;
 do
     np=`echo "$n*10^3" | bc` &&
-    echo "srun python train_mnist.py --nepochs 6000  --num_parameters $np --vary_name size_max num_parameters  --no-softmax --size_max 4000 --learning_rate 0.01" >> $fname; 
+    echo "#srun python train_mnist.py --nepochs 6000  --num_parameters $np --vary_name size_max num_parameters  --no-softmax --size_max 4000 --learning_rate 0.01" >> $fname; 
     
 done;
 
-sbatch $fname
+nexp=`grep srun $fname  | wc -l`  # the number of experiments in the file
+total=`wc -l < $fname`  # the total number of lines
+
+max_run=1
+
+let beg=$total-$nexp+1
+let i=$beg
+
+let blocks=($nexp - 1)/$max_run+1
+
+for bcnt in `seq 1 $blocks`; do
+    sed -i "$i,`expr $i + $max_run - 1`s/^#*//" $fname
+    sbatch $fname
+    sed -i "$i,`expr $i + $max_run - 1`s/^/#/" $fname
+    let i=$i+$max_run
+done;
+
+
+
 
